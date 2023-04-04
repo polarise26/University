88b30d6ab3cd715dbf2e6a816a46b0cefd590360
polarise26 <ghostwolfhhh@gmail.com>
2023-04-04 22:58:50 +0530

    Routine updation (after a LONG time)

---
+++ year-3/sem-5/algo-intl/exp3.py
@@ -0,0 +1,16 @@
+def linear_search(alist, key):
+    for i in range(len(alist)):
+        if alist[i] == key:
+            return i
+    return -1
+
+
+alist = input('Enter the list of numbers: ')
+alist = alist.split()
+alist = [int(x) for x in alist]
+key = int(input('The number to search for: '))
+index = linear_search(alist, key)
+if index < 0:
+    print('{} was not found.'.format(key))
+else:
+    print('{} was found at index {}.'.format(key, index))
